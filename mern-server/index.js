8c99c42ff05c2664689bc2911f112fc2d9c1a519
quratulain08 <141954239+quratulain08@users.noreply.github.com>
2024-07-29 20:31:16 +0500

    Backend Done

--- mern-server/index.js
+++ mern-server/index.js
@@ -16,7 +16,7 @@ app.get('/', (req,res)=>{
 
 //connection string
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://Bookstore-mern:0ekzZ6ZeloED9Y0J@cluster0.phhmzyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,6 +32,75 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+   //create a collection of documents
+   const bookCollections= client.db("BookInventory").collection("books");
+
+   //Insert a  Book to the database. Post method
+   app.post("/upload-book",async(req,res)=>{
+    const data =  req.body;
+    const result = await bookCollections.insertOne(data);
+    res.send(result);
+   })
+
+
+ //get all books
+ //app.get("/all-books",async(req, res)=>{
+   // const books = bookCollections.find();
+    //const result = await books.toArray();
+    //res.send(result);
+
+ //})
+
+ //update the books : patch or update method
+ app.patch("/book/:id", async(req,res)=>{
+    const id = req.params.id;
+   // console.log(id);
+   const updateBookData =  req.body;
+   const filter = {_id: new ObjectId(id)};
+   const options = {upsert: true};
+
+   const updateDoc = {
+    $set:{
+        ...updateBookData
+    }
+   }
+   //update
+   const result = await bookCollections.updateOne(filter,updateDoc,options);
+   res.send(result);
+ })
+
+ //delete method for deleting a book
+app.delete("/book/:id",async(req,res)=>{
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)};
+    const result = await bookCollections.deleteOne(filter);
+    res.send(result);
+
+})
+
+//findbycategory
+
+app.get("/all-books", async (req, res) => {
+    try {
+      let query = {};
+  
+      // Check if 'category' query parameter is provided
+      if (req.query?.category) {
+        query = { category: req.query.category }
+      }
+  
+      // Fetch the books based on the query
+      const result = await bookCollections.find(query).toArray();
+  
+      // Send the result back to the client
+      res.send(result);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+      res.status(500).send({ error: 'Failed to fetch books' });
+    }
+  });
+  
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
